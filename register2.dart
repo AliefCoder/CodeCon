ecda366b778b02c1cca97b960ae684fe0659f666
adibzqis <147358820+adibzqis@users.noreply.github.com>
2024-10-24 22:41:56 +0800

    Update register2.dart

--- register2.dart
+++ register2.dart
@@ -8,7 +8,39 @@ class RegisterPage2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Registeration'),
+        title: Text(
+          'Registeration',
+          style: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+          ),
+          backgroundColor: Colors.green[600],
+          centerTitle: true,    
+      ),
+      body: Container(
+        width: double.infinity,
+        color: Colors.orange[600],
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(5, 0, 100, 0),
+              child: Text(
+                'We want to know more about your body!',
+                style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
