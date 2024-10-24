81a8b17ec370154ab099c9df61412b40cad5b344
adibzqis <147358820+adibzqis@users.noreply.github.com>
2024-10-24 10:37:29 +0800

    Add files via upload

---
+++ register2.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class RegisterPage2 extends StatelessWidget {
+  const RegisterPage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registeration'),
+      ),
+    );
+  }
+}
